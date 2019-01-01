64b6438cfe1fc52b22f36bc7446cf686793ee03d
DeokJo Jang <jdj9354@naver.com>
2019-01-01 16:13:35 +0900

    Change cursor to pointer on Grid Item & potential bug fix for topNav button size

--- src/containers/layout/grid/ShopItem.js
+++ src/containers/layout/grid/ShopItem.js
@@ -5,7 +5,7 @@ class ShopItem extends Component{
 
     render(){
         if(this.props.product)
-            return (<div onClick={() => window.location.href= "/detail/"+this.props.product._id}>
+            return (<div className="ShopItem" onClick={() => window.location.href= "/detail/"+this.props.product._id}>
                         <img src={this.props.product.thumbnailImageSrc}/><br></br>
                         {this.props.product.name}<br></br>
                         {this.props.product.price} {this.props.product.priceUnit}<br></br>
