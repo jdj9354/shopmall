0c9bc36b6b63aee4c08a62a5f5d7dba56c0377b7
unknown <jdj9354@gmail.com>
2019-01-28 01:29:16 +0900

    Add search Frontend End Point & Implement Search Result Layout

--- src/containers/element/RoundedSearchBar.js
+++ src/containers/element/RoundedSearchBar.js
@@ -6,17 +6,13 @@ let backendController = new BackendController();
 
 class RoundedSearchBar extends Component {
 
-    async searchRequest(query){
-        return await backendController.searchProduct(query,1,10);
-    }
-
     render() {
         return (
             <div className="RoundedSearchBar">
                 <div className="searchBarWrapper">
                     <input onKeyDown={async (event)=>{
                         if(event.keyCode == 13){
-                            let result = await this.searchRequest(event.target.value);
+                            window.location.href='/search/'+event.target.value;
                         }
                     }} type="text"></input>
                 </div>
--- src/containers/layout/SearchResult.js
+++ src/containers/layout/SearchResult.js
@@ -1,32 +1,109 @@
 import React, {Component} from 'react';
 import './SearchResult.css';
 import BackendController from "../../controller/BackendController";
+import ShopItemRow from "./grid/ShopItemRow";
+
+let backendController = new BackendController();
 
 class SearchResult extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
-            searchQuery: props.searchQuery
+            searchQuery: props.searchQuery,
             page: 1,
             pageLimit: 10,
             resultItems: [],
             totalPage: 1
         }
 
-        BackendController.
-
         this.setState(this.state);
+        this.requestSearchList(1);
     }
 
-    render(){
-        return(
-            <div className="SearchResult">
+    async requestSearchList(page){
+        let result = await backendController.searchProduct(this.state.searchQuery, page, this.state.pageLimit);
+        if(result != null) {
+            result = result.searchResult;
+            this.setState({
+                resultItems: result.docs,
+                page: page,
+                totalPage: result.pages
+            });
+        }
+    }
 
+    render() {
+
+        let products = this.state.resultItems;
+        let productsRow = [];
+        let columnSize = this.props.col? this.props.col : 3;
+        for(let i=0; i<products.length; i+=columnSize){
+            let start = i;
+            let end = i+columnSize >= products.length ? products.length  : i+columnSize;
+
+            productsRow.push(products.slice(start,end));
+        }
+
+        let pageStart = Math.floor(this.state.page / 10) * 10 + 1;
+        let pageEnd = pageStart + 9 >= this.state.totalPage ? this.state.totalPage : pageStart + 9;
+        let pageTailText = [];
+        for (let i = pageStart; i <= pageEnd; i++) {
+            pageTailText.push(i);
+        }
+
+        let count = 0;
+
+        return (
+            <div className="SearchResult">
+                <div className="itemContainer">
+                    {
+                        productsRow.map((el) => {
+                            return <ShopItemRow products={el}/>;
+                        })
+                    }
+                </div>
+                <div className="footerWrapper">
+                    {
+                        <div className="arrow arrow-left" onClick={(event) => {
+                            let targetIdx = Math.floor(this.state.page / 10) * 10;
+                            if (targetIdx <= 1)
+                                targetIdx = 1;
+                            this.requestSearchList(targetIdx);
+                        }
+                        }></div>
+                    }
+                    <div>
+                        {
+                            pageTailText.map((text) => {
+                                if (++count == this.state.page)
+                                    return (
+                                        <div className="selected"><strong>{text}</strong></div>
+                                    )
+                                else {
+                                    return (
+                                        <div onClick={(event) => {
+                                            this.requestSearchList(event.target.innerText);
+                                        }
+                                        }>{text}</div>
+                                    )
+                                }
+                            })
+                        }
+                    </div>
+                    {
+                        <div className="arrow arrow-right" onClick={(event) => {
+                            let targetIdx = (Math.floor(this.state.page / 10) + 1) * 10;
+                            if (targetIdx >= this.state.totalPage)
+                                targetIdx = this.state.totalPage;
+                            this.requestSearchList(targetIdx);
+                        }}></div>
+                    }
+                </div>
             </div>
         )
     }
 }
 
-export default SearchResult
+export default SearchResult;
